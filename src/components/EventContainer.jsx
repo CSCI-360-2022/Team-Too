266ffe91fec161e1a75a39f24cd1b2420c7a8086
jlloyd647 <jlloyd647@gmail.com>
2022-11-29 19:10:02 -0500

    added svg

--- src/components/EventContainer.jsx
+++ src/components/EventContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import seating from '../images/Seating2.PNG'
+import startSeat from '../images/startSeat.svg'
 import Ticket from './Ticket'
 import { seatData } from '../__mocks__/mockdata'
 import './styles.css'
@@ -26,11 +27,14 @@ function EventContainer({selectedEvent, cartFunctions}) {
       </Row>
       <Row>
       <Col>
-          <img
-            className="d-block w-100"
-            src={seating}
-            alt="First slide"
-          />
+      <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 225.58 223.5">
+        <defs>
+        </defs>
+        <rect class="cls-1" x=".5" y=".5" width="103.97" height="103.97" fill={'#4ADEDE'} onClick={() => console.log('clicked')}/>
+        <rect class="cls-1" x="121.11" y=".5" width="103.97" height="103.97"/>
+        <rect class="cls-1" x=".5" y="119.03" width="103.97" height="103.97"/>
+        <rect class="cls-1" x="121.11" y="119.03" width="103.97" height="103.97"/>
+      </svg>
           {
             seatList.map((seat) => {
               return (
